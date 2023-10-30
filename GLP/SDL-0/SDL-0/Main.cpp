bcbb194b0c46d02ee6a016bb3e2d04c0f8b5ec13
Loris M <80474693+Loris-Moreau@users.noreply.github.com>
2023-10-30 16:51:38 +0100

    Shader file

--- GLP/SDL-0/SDL-0/Main.cpp
+++ GLP/SDL-0/SDL-0/Main.cpp
@@ -1,160 +1,183 @@
-﻿#include <iostream>
-#include <SDL.h>
+﻿#include <SDL.h>
 #include <glew.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
 using namespace std;
-//#define GLEW_STATIC
 
-int main(int argc, char* argv[])
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-	{
-		cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
-	}
-	else
-	{
-		cout << "SDL initialization succeeded!\n";
-	}
-	///////////SETTING UP SDL/////////////
-	//Create a simple window
-	int width = 400;
-	int height = 300;
-	unsigned int center = SDL_WINDOWPOS_CENTERED;
-	SDL_Window* Window = SDL_CreateWindow("My window", center, center, width, height, SDL_WINDOW_OPENGL);
-	//SDL_WINDOW_OPENGL is a u32 flag !
+string LoadShader(string fileName);
 
+int main(int argc = 0, char** argv = nullptr) {
 
-	//Create an OpenGL compatible context to let glew draw on it
-	SDL_GLContext Context = SDL_GL_CreateContext(Window);
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        cout << "SDL initialized successfully \n";
+    }
 
-	/////////SETTING UP OPENGL WITH GLEW///
-	//Initialize glew
-	glewExperimental = GL_TRUE;
-	if (glewInit() == GLEW_OK) {
-		cout << "Glew initialized successfully\n";
-	}
-	//Set the viewing frame through which we will see the objects
-	glViewport(0, 0, width, height);
+    int flags = SDL_WINDOW_OPENGL;
 
-	//Put the color you want here for the background
-	glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
-
-	//Describe the shape by its vertices
-
-	float vertices[] = 
-	{
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
-	};
-
-	// Create an ID to be given at object generation
-		unsigned int vbo;
-
-	//Pass how many buffers should be created and the reference of the ID to get the value set
-	glGenBuffers(1, &vbo);
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        cout << "SDL failed to initialize" << endl;
+        return 1;
+    }
+    ///////////SETTING UP SDL/////////////
+    //Create a simple window
+    int width = 640;
+    int height = 480;
+    unsigned int center = SDL_WINDOWPOS_CENTERED;
+    SDL_Window* Window = SDL_CreateWindow("My window", center, center, width, height, SDL_WINDOW_OPENGL);
+    //SDL_WINDOW_OPENGL is a u32 flag !
 
 
-	const char* vertexShaderSource = "#version 330 core\n"
-		"in vec3 pos;\n"
-		"void main()\n"
-		"{\n"
-		"   gl_Position = vec4(pos, 1.0);\n"
-		"}\0";
+    ////Create an OpenGL compatible context to let glew draw on it
+    SDL_GLContext Context = SDL_GL_CreateContext(Window);
 
 
-	const char* fragmentShaderSource = "#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"void main()\n"
-		"{\n"
-		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-		"}\n\0";
-
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    /////////SETTING UP OPENGL WITH GLEW///
+    //Initialize glew
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        cout << "Glew failed to initialize\n";
+    }
 
 
-	//now that we have a vertex shader, let’s put the code text inside
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    // Get info
+    const GLubyte* renderer = glGetString(GL_RENDERER);
+    const GLubyte* version = glGetString(GL_VERSION);
+    cout << "Renderer: " << renderer << endl;
+    cout << "OpenGL version supported: " << version << endl;
+
+    // Tell GL to only draw onto a pixel if the shape is closer to the viewer
+    glEnable(GL_DEPTH_TEST); // enable depth-testing
+    glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+    // Set viewport and clear color
+    glViewport(0, 0, width, height);
+    glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
+
+    // Load
+
+    //Describe the shape by its vertices
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    //Create an ID to be given at object generation
+    unsigned int vbo = 0;
 
 
-	//aaaaand… Compile !
-	glCompileShader(vertexShader);
+    //Pass how many buffers should be created and the reference of the ID to get the value set
+    glGenBuffers(1, &vbo);
 
 
-	//Do the same with the fragment shader !
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
+    //Binds the buffer linked to this ID to the vertex array buffer to be rendered. Put 0 instead of vbo to reset the value.
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
+    //Finally send the vertices array in the array buffer (linked to vbo)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    string vs = LoadShader("vertex.shader");
+    const char* vertexShaderSource = vs.c_str();
+    string fs = LoadShader("fragment.shader");
+    const char* fragmentShaderSource = fs.c_str();
 
-
-	//now attach shaders to use to the program
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-
-	//and link it 
-	glLinkProgram(shaderProgram);
-
-	//now that the program is complete, we can use it 
-	glUseProgram(shaderProgram);
-
-	//Create one ID to be given at object generation
-	unsigned int vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
 
-	//Binds the buffer linked to this ID to the vertex array buffer to be rendered. Put 0 instead of vbo to reset the value.
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    //now that we have a vertex shader, let’s put the code text inside
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 
-	//Finally send the vertices array in the array buffer 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	//Enable my vertex attrib array number 0 (we only have one attribute of position)
-	glEnableVertexAttribArray(0);
+    //aaaaand… Compile !
+    glCompileShader(vertexShader);
 
-	//Use depth management
-	glEnable(GL_DEPTH_TEST);
 
-	//0 is our origin, the higher the z, the farther the object
-	glDepthFunc(GL_LESS);
+    //Do the same with the fragment shader !
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
 
-	bool isRunning = true;
-	while (isRunning) {
-		// Inputs
-		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			switch (event.type) {
-			case SDL_QUIT:
-				isRunning = false;
-				break;
-			default:
-				break;
-			}
-		}
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
 
-		//Shader to use next
-		glUseProgram(shaderProgram);
 
-		//VAO to use next
-		glBindVertexArray(vao);
 
-		//OMG WE FINALLY DRAW ! We use the GL_TRIANGLES primitive type
-		//We draw from vertex 0 and we will be drawing 3 vertices
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+    //now attach shaders to use to the program
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
 
-		SDL_GL_SwapWindow(Window); // Swapbuffer
-	}
-	// Quit
-	SDL_DestroyWindow(Window);
-	SDL_GL_DeleteContext(Context);
+    //and link it 
+    glLinkProgram(shaderProgram);
 
-	cin.get();
-	return 0;
+    //now that the program is complete, we can use it 
+    glUseProgram(shaderProgram);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    // ..:: Initialization code (done once (unless your object frequently changes)) :: ..
+    // 1. bind Vertex Array Object
+    glBindVertexArray(VAO);
+    // 2. copy our vertices array in a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 3. then set our vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Game loop
+    bool isRunning = true;
+    while (isRunning) {
+        // Inputs
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT:
+                isRunning = false;
+                break;
+
+            default:
+                break;
+            }
+        }
+        // Update
+
+
+        // Draw
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen
+
+        // Draw here
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        SDL_GL_SwapWindow(Window); // Swapbuffer
+    }
+
+
+    // Quit
+    SDL_DestroyWindow(Window);
+    SDL_GL_DeleteContext(Context);
+
+    return 0;
+}
+
+
+string LoadShader(string fileName) {
+    ifstream myFile;
+    myFile.open(fileName);
+    if (myFile.fail()) {
+        cerr << "Error - failed to open " << fileName << endl;
+    }
+    string fileText = "";
+    string line = "";
+    while (getline(myFile, line)) {
+        fileText += line + '\n';
+    }
+    return fileText;
 }
